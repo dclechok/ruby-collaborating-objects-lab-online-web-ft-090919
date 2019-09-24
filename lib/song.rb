cf04936865c4051ee92b007a1bd9d0d5b2741e35
Daniel Lechok <dclechok@gmail.com>
2019-09-24 18:27:16 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self  
+  end
+  
+  def artist=()
+      artist = self.name
+  end
+  
+  def self.all
+    @@all
+  end
+end
